324cea828574ccdcab43ccb74fce3f023913fe68
David L. Parsley <parsley@linuxjedi.org>
2016-09-13 15:00:06 -0400

    First working version of route53 tool with arec / txt / cname records

--- lib/cloud.rb
+++ lib/cloud.rb
@@ -30,31 +30,54 @@ module RAWSTools
 	class Route53
 		def initialize(cloudmgr)
 			@mgr = cloudmgr
-			@route53 = Aws::Route53::Client.new( region: @mgr["Region"] )
+			@client = Aws::Route53::Client.new( region: @mgr["Region"] )
 		end
 
-		def r53_set(where, template)
+		def change_records(where, template, wait = false)
 			templatefile = nil
 			if File::exist?("#{template}.json")
 				templatefile = "#{template}.json"
 			else
-				templatefile = "#{@installdir}/defaults/route53/#{template}.json"
+				templatefile = "#{@mgr.installdir}/defaults/route53/#{template}.yaml"
 			end
 			set = YAML::load(File::read(templatefile))
-			case where
-			when :public
-				set["hosted_zone_id"] = @config["PublicDNSId"]
-			when :private
-				set["hosted_zone_id"] = @config["PrivateDNSId"]
+
+			name = @mgr.getparam("name")
+			if not name.end_with?(@mgr["DNSDomain"])
+				@mgr.setparam("name", "#{name}.#{@mgr["DNSDomain"]}")
 			end
-			resolve_vars( { "child" => set }, "child" )
+			cname = @mgr.getparam("cname")
+			if cname and not cname.end_with?(@mgr["DNSDomain"])
+				@mgr.setparam("cname", "#{cname}.#{@mgr["DNSDomain"]}")
+			end
+
+			@mgr.resolve_vars( { "child" => set }, "child" )
+			@mgr.symbol_keys(set)
+
+			if where == :public or where == :both
+				set[:hosted_zone_id] = @mgr["PublicDNSId"]
+				pubresp = @client.change_resource_record_sets(set)
+			end
+			if where == :private or where == :both
+				set[:hosted_zone_id] = @mgr["PrivateDNSId"]
+				privresp = @client.change_resource_record_sets(set)
+			end
+
+			return unless wait
+
+			if where == :public or where == :both
+				@client.wait_until(:resource_record_sets_changed, id: pubresp.change_info.id )
+			end
+			if where == :private or where == :both
+				@client.wait_until(:resource_record_sets_changed, id: privresp.change_info.id )
+			end
 		end
 	end
 
 	# For reading in the configuration file and initializing service clients
 	# and resources
 	class CloudManager
-		attr_reader :installdir, :cfn, :cfnres, :s3, :ec2, :ec2res, :route53
+		attr_reader :installdir, :cfn, :cfnres, :s3, :s3res, :ec2, :ec2res, :route53
 
 		def initialize(filename, installdir)
 			@filename = filename
@@ -78,10 +101,12 @@ module RAWSTools
 			@config["tags"] = tags.loweroutput
 
 			@cfn = Aws::CloudFormation::Client.new( region: @config["Region"] )
-			@cfnres = Aws::CloudFormation::Resource.new( client: cfn )
-			@s3 = Aws::S3::Resource.new( region: @config["Region"] )
+			@cfnres = Aws::CloudFormation::Resource.new( client: @cfn )
+			@s3 = Aws::S3::Client.new( region: @config["Region"] )
+			@s3res = Aws::S3::Resource.new( client: @s3 )
 			@ec2 = Aws::EC2::Client.new( region: @config["Region"] )
 			@ec2res = Aws::EC2::Resource.new(client: @ec2)
+			@route53 = Route53.new(self)
 		end
 
 		def setparams(hash)
@@ -100,11 +125,28 @@ module RAWSTools
 			@config[key]
 		end
 
+		def symbol_keys(item)
+			case item.class().to_s()
+			when "Hash"
+				keys = item.keys()
+				keys.each() do |key|
+					if key.class.to_s() == "String"
+						symkey = key.to_sym()
+						item[symkey] = item[key]
+						item.delete(key)
+					end
+					symbol_keys(item[symkey])
+				end
+			when "Array"
+				item.each() { |i| symbol_keys(i) }
+			end
+		end
+
 		def resolve_value(var)
 			cfgvar = var[1..-1]
 			case cfgvar[0]
 			when "@"
-				return get(cfgvar[1..-1])
+				return getparam(cfgvar[1..-1])
 			else
 				if @config[cfgvar] == nil
 					raise "Bad variable reference: \"#{cfgvar}\" not defined in #{@filename}"
